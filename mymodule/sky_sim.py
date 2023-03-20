71e5385017f3ce59669a296b17f662f7caf64c44
PaulHancock <paul.hancock@curtin.edu.au>
2023-03-20 14:09:08 +1100

    rename constants, fixes C0103

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -5,23 +5,23 @@ import math
 import random
 
 # from wikipedia
-ra = '00:42:44.3'
-dec = '41:16:09'
+RA = '00:42:44.3'
+DEC = '41:16:09'
 
-d, m, s = dec.split(':')
+d, m, s = DEC.split(':')
 dec = int(d)+int(m)/60+float(s)/3600
 
-h, m, s = ra.split(':')
+h, m, s = RA.split(':')
 ra = 15*(int(h)+int(m)/60+float(s)/3600)
 ra = ra/math.cos(dec*pi/180)
 
-nsrc = 1_000_000
+NSRC = 1_000_000
 
 # make 1000 stars within 1 degree of Andromeda
 
 ras = []
 decs = []
-for i in range(nsrc):
+for i in range(NSRC):
     ras.append(ra + random.uniform(-1,1))
     decs.append(dec + random.uniform(-1,1))
 
@@ -29,5 +29,5 @@ for i in range(nsrc):
 # now write these to a csv file for use by my other program
 f = open('catalog.csv','w')
 print("id,ra,dec", file=f)
-for i in range(nsrc):
+for i in range(NSRC):
     print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
